2e3a6a9183f6e009ce2016abb55ec86c5126bf67
Vivek Bhupatiraju <vivekab@mit.edu>
2023-05-14 01:54:15 +0200

    added cron job

--- apps/zupoll-server/src/application.ts
+++ apps/zupoll-server/src/application.ts
@@ -2,11 +2,17 @@ import { startServer } from "./routing/server";
 import { ServiceInitializer } from "./services/types";
 import { ApplicationContext } from "./types";
 import { Bot } from "grammy";
+import { CronJob } from "cron";
+import { prisma } from "./util/prisma";
+import { SITE_URL } from "./util/auth";
+import { cleanString, sendMessage } from "./util/bot";
 
 const services: ServiceInitializer[] = [startServer];
 
 export async function startApplication() {
   let context: ApplicationContext = {};
+
+  // start up bot
   const botToken = process.env.BOT_TOKEN;
   if (botToken !== undefined) {
     context = {
@@ -14,6 +20,86 @@ export async function startApplication() {
     };
     context.bot?.start();
   }
+
+  // start up cron jobs
+  const cronJob = new CronJob(
+    "0,15,30,45 * * * *", // every 15 minutes
+    async () => {
+      const ballots = await prisma.ballot.findMany({
+        select: {
+          ballotTitle: true,
+          ballotURL: true,
+          expiry: true,
+          expiryNotif: true,
+        },
+        orderBy: { expiry: "desc" },
+      });
+
+      for (const ballot of ballots) {
+        const hours = Math.ceil(
+          (new Date(ballot.expiry).getTime() - Date.now()) / (1000 * 60 * 60)
+        );
+        const days = Math.ceil(hours / 24);
+
+        const sharedMessage = `\n\nVote at ${SITE_URL}ballot?id=${ballot.ballotURL}`;
+
+        if (days === 7 && ballot.expiryNotif === "NONE") {
+          await prisma.ballot.update({
+            where: {
+              ballotURL: ballot.ballotURL,
+            },
+            data: {
+              expiryNotif: "WEEK",
+            },
+          });
+
+          const expiryMessage = `<b>${cleanString(
+            ballot.ballotTitle
+          )}</b> will expire in <1 week.${sharedMessage}`;
+          await sendMessage(expiryMessage, context.bot);
+
+        } else if (
+          days === 1 &&
+          (ballot.expiryNotif === "WEEK" || ballot.expiryNotif === "NONE")
+        ) {
+          await prisma.ballot.update({
+            where: {
+              ballotURL: ballot.ballotURL,
+            },
+            data: {
+              expiryNotif: "DAY",
+            },
+          });
+
+          const expiryMessage = `<b>${cleanString(
+            ballot.ballotTitle
+          )}</b> will expire in <24 hours.${sharedMessage}`;
+          await sendMessage(expiryMessage, context.bot);
+
+        } else if (
+          hours === 1 &&
+          (ballot.expiryNotif === "DAY" || ballot.expiryNotif === "NONE")
+        ) {
+          await prisma.ballot.update({
+            where: {
+              ballotURL: ballot.ballotURL,
+            },
+            data: {
+              expiryNotif: "HOUR",
+            },
+          });
+
+          const expiryMessage = `<b>${cleanString(
+            ballot.ballotTitle
+          )}</b> will expire in <1 hour!${sharedMessage}`;
+          await sendMessage(expiryMessage, context.bot);
+        }
+      }
+    },
+  );
+  
+  cronJob.start();
+
   for (const service of services) {
     await service(context);
   }
--- apps/zupoll-server/src/util/bot.ts
+++ apps/zupoll-server/src/util/bot.ts
@@ -7,6 +7,8 @@ export function cleanString(str: string) {
 }
 
 export async function sendMessage(message: string, bot?: Bot) {
+  console.log(message);
+  
   if (bot !== undefined) {
     const supergroup_id = process.env.BOT_SUPERGROUP_ID;
     const channel_id = process.env.BOT_CHANNEL_ID;
@@ -14,7 +16,6 @@ export async function sendMessage(message: string, bot?: Bot) {
     if (supergroup_id !== undefined && channel_id !== undefined) {
       const message_thread_id = parseInt(channel_id);
       const chat_id = parseInt(supergroup_id)
-      console.log("sending message to", chat_id, message_thread_id);
       if (!isNaN(message_thread_id) && !isNaN(chat_id)) {
         await bot.api.sendMessage(chat_id, message, {
           message_thread_id,
