021398f2ede75ceff530b4bb560de2e79d753ec8
Ivan Chub <ivansergchub@gmail.com>
2023-07-05 18:40:33 +0100

    removed some console.logs

--- apps/zupoll-server/src/main.ts
+++ apps/zupoll-server/src/main.ts
@@ -9,16 +9,6 @@ const dotEnvPath = IS_DEPLOYED
 
 console.log(`[INIT] Loading environment variables from: ${dotEnvPath} `);
 dotenv.config({ path: dotEnvPath });
-
-console.log(
-  "ZUZALU_PARTICIPANTS_GROUP_URL=",
-  process.env.ZUZALU_PARTICIPANTS_GROUP_URL
-);
-console.log(
-  "ZUZALU_ORGANIZERS_GROUP_URL=",
-  process.env.ZUZALU_ORGANIZERS_GROUP_URL
-);
-
 console.log("[INIT] Starting application");
 
 import { startApplication } from "./application";
