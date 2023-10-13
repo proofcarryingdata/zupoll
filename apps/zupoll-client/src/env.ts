c9d9bedea699aa32d97fd5e702d4ad7b711e9594
Rob Knight <mail@robknight.org.uk>
2023-10-13 11:29:34 -0700

    Resolve Semaphore dependencies to more recent versions

--- apps/zupoll-client/src/env.ts
+++ apps/zupoll-client/src/env.ts
@@ -31,7 +31,7 @@ const pcdpassServerUrl: Record<DEPLOYMENT_TYPE, string> = {
 const zupollServerURL: Record<DEPLOYMENT_TYPE, string> = {
   [DEPLOYMENT_TYPE.PROD]: "https://api.zupoll.org/",
   [DEPLOYMENT_TYPE.STAGING]: "https://api-staging.zupoll.org/",
-  [DEPLOYMENT_TYPE.LOCAL]: "http://localhost:3005/",
+  [DEPLOYMENT_TYPE.LOCAL]: "http://localhost:3105/",
 };
 
 export const DEPLOYMENT: DEPLOYMENT_TYPE =
