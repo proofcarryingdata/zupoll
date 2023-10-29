7fb65719b03e6367e91607ca2914db7fc16a2487
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-29 18:43:44 +0300

    feat: explore more tg-native poll features

--- apps/zupoll-server/src/bot.ts
+++ apps/zupoll-server/src/bot.ts
@@ -1,10 +1,88 @@
 import { BallotType } from "@prisma/client";
-import { CronJob } from "cron";
-import { Bot } from "grammy";
+import { Api, Bot, Context, InlineKeyboard, RawApi } from "grammy";
 import { ApplicationContext } from "./types";
 import { SITE_URL } from "./util/auth";
-import { cleanString, sendMessage } from "./util/bot";
-import { sleep } from "./util/util";
+import { cleanString, formatPollCreated, sendMessage } from "./util/bot";
+
+const findBallots = async (bot: Bot<Context, Api<RawApi>>) => {
+  console.log(`Running find ballots: ${Date.now()}`);
+  const ballots = await prisma.ballot.findMany({
+    select: {
+      ballotTitle: true,
+      ballotURL: true,
+      expiry: true,
+      expiryNotif: true,
+    },
+    orderBy: { expiry: "desc" },
+    where: {
+      NOT: {
+        ballotType: {
+          in: [BallotType.PCDPASSUSER, BallotType.ORGANIZERONLY],
+        },
+      },
+    },
+  });
+
+  for (const ballot of ballots) {
+    const minutes = Math.ceil(
+      (new Date(ballot.expiry).getTime() - Date.now()) / 60000
+    );
+    const hours = Math.ceil(minutes / 60);
+    const days = Math.ceil(minutes / (24 * 60));
+
+    const pollUrl = `${SITE_URL}ballot?id=${ballot.ballotURL}`;
+
+    if (days === 7 && ballot.expiryNotif === "NONE") {
+      await prisma.ballot.update({
+        where: {
+          ballotURL: ballot.ballotURL,
+        },
+        data: {
+          expiryNotif: "WEEK",
+        },
+      });
+
+      const expiryMessage = `<b>${cleanString(
+        ballot.ballotTitle
+      )}</b> will expire in less than 1 week. Vote at ${pollUrl}`;
+      await sendMessage(expiryMessage, bot);
+    } else if (
+      hours === 24 &&
+      (ballot.expiryNotif === "WEEK" || ballot.expiryNotif === "NONE")
+    ) {
+      await prisma.ballot.update({
+        where: {
+          ballotURL: ballot.ballotURL,
+        },
+        data: {
+          expiryNotif: "DAY",
+        },
+      });
+
+      const expiryMessage = `<b>${cleanString(
+        ballot.ballotTitle
+      )}</b> will expire in less than 24 hours. Vote at ${pollUrl}`;
+      await sendMessage(expiryMessage, bot);
+    } else if (
+      hours === 1 &&
+      (ballot.expiryNotif === "DAY" || ballot.expiryNotif === "NONE")
+    ) {
+      await prisma.ballot.update({
+        where: {
+          ballotURL: ballot.ballotURL,
+        },
+        data: {
+          expiryNotif: "HOUR",
+        },
+      });
+
+      const expiryMessage = `<b>${cleanString(
+        ballot.ballotTitle
+      )}</b> will expire in less than 1 hour! Get your votes in at ${pollUrl}`;
+      await sendMessage(expiryMessage, bot);
+    }
+  }
+};
 
 export async function startBot(context: ApplicationContext): Promise<void> {
   const botToken = process.env.BOT_TOKEN;
@@ -13,98 +91,65 @@ export async function startBot(context: ApplicationContext): Promise<void> {
     return;
   }
 
-  console.log("starting bot");
-
   // there can only be one bot active at a time - give the old service
   // some time to stop before starting the bot
-  await sleep(30 * 1000);
-
+  // await sleep(30 * 1000);
   context.bot = new Bot(botToken);
-  context.bot.start();
 
-  // start up cron jobs
-  const cronJob = new CronJob(
-    "0,15,30,45 * * * *", // every 15 minutes, check if any ballots are expiring soon
-    async () => {
-      const ballots = await prisma.ballot.findMany({
-        select: {
-          ballotTitle: true,
-          ballotURL: true,
-          expiry: true,
-          expiryNotif: true,
-        },
-        orderBy: { expiry: "desc" },
-        where: {
-          NOT: {
-            ballotType: {
-              in: [BallotType.PCDPASSUSER, BallotType.ORGANIZERONLY],
-            },
+  context.bot.command("start", async (ctx) => {
+    ctx.reply(`Zupoll`, {
+      reply_markup: new InlineKeyboard().webApp(`Zupoll`, `https://zupoll.org`),
+    });
+  });
+
+  context.bot.command("latest", async (ctx) => {
+    const ballots = await prisma.ballot.findMany({
+      select: {
+        ballotTitle: true,
+        ballotURL: true,
+        expiry: true,
+        expiryNotif: true,
+        ballotDescription: true,
+        polls: true,
+      },
+      orderBy: { expiry: "desc" },
+      where: {
+        NOT: {
+          ballotType: {
+            in: [BallotType.PCDPASSUSER, BallotType.ORGANIZERONLY],
           },
         },
-      });
-
-      for (const ballot of ballots) {
-        const minutes = Math.ceil(
-          (new Date(ballot.expiry).getTime() - Date.now()) / 60000
-        );
-        const hours = Math.ceil(minutes / 60);
-        const days = Math.ceil(minutes / (24 * 60));
-
-        const pollUrl = `${SITE_URL}ballot?id=${ballot.ballotURL}`;
-
-        if (days === 7 && ballot.expiryNotif === "NONE") {
-          await prisma.ballot.update({
-            where: {
-              ballotURL: ballot.ballotURL,
-            },
-            data: {
-              expiryNotif: "WEEK",
-            },
-          });
-
-          const expiryMessage = `<b>${cleanString(
-            ballot.ballotTitle
-          )}</b> will expire in less than 1 week. Vote at ${pollUrl}`;
-          await sendMessage(expiryMessage, context.bot);
-        } else if (
-          hours === 24 &&
-          (ballot.expiryNotif === "WEEK" || ballot.expiryNotif === "NONE")
-        ) {
-          await prisma.ballot.update({
-            where: {
-              ballotURL: ballot.ballotURL,
-            },
-            data: {
-              expiryNotif: "DAY",
-            },
-          });
-
-          const expiryMessage = `<b>${cleanString(
-            ballot.ballotTitle
-          )}</b> will expire in less than 24 hours. Vote at ${pollUrl}`;
-          await sendMessage(expiryMessage, context.bot);
-        } else if (
-          hours === 1 &&
-          (ballot.expiryNotif === "DAY" || ballot.expiryNotif === "NONE")
-        ) {
-          await prisma.ballot.update({
-            where: {
-              ballotURL: ballot.ballotURL,
-            },
-            data: {
-              expiryNotif: "HOUR",
-            },
-          });
-
-          const expiryMessage = `<b>${cleanString(
-            ballot.ballotTitle
-          )}</b> will expire in less than 1 hour! Get your votes in at ${pollUrl}`;
-          await sendMessage(expiryMessage, context.bot);
-        }
-      }
+      },
+    });
+    console.log(`[BALLOTS]`, ballots);
+    for (const ballot of ballots.slice(0, 1)) {
+      // @ts-expect-error prisma
+      const post = formatPollCreated(ballot, ballot.polls);
+      await sendMessage(post, context.bot);
     }
-  );
+    // await ctx.reply(`Check console for ballots`, {
+    //   message_thread_id: ctx.message?.message_thread_id,
+    // });
+    // List most recent ballots
+  });
 
-  cronJob.start();
+  await context.bot.start({
+    onStart(info) {
+      console.log(`[TELEGRAM] Started bot '${info.username}' successfully!`);
+    },
+  });
+
+  // start up cron jobs
+  // const cronJob = new CronJob(
+  //   "0,15,30,45 * * * *", // every 15 minutes, check if any ballots are expiring soon
+  //   async () => {
+  //     if (context.bot) {
+  //       await findBallots(context.bot);
+  //     }
+  //   }
+  // );
+
+  // cronJob.start();
+  // const res = setInterval(findBallots, 1000 * 10);
   console.log("started bot");
 }
--- apps/zupoll-server/src/util/bot.ts
+++ apps/zupoll-server/src/util/bot.ts
@@ -1,4 +1,7 @@
+import { Ballot, BallotType, Poll } from "@prisma/client";
 import { Bot } from "grammy";
+
+export const SITE_URL = process.env.SITE_URL ?? "https://zupoll.org/";
 
 // Removing characters that mess up the HTML parsing of Telegram, as per
 // https://core.telegram.org/bots/api#html-style
@@ -8,14 +11,14 @@ export function cleanString(str: string) {
 
 export async function sendMessage(message: string, bot?: Bot) {
   console.log(message);
-  
+
   if (bot !== undefined) {
     const supergroup_id = process.env.BOT_SUPERGROUP_ID;
     const channel_id = process.env.BOT_CHANNEL_ID;
 
     if (supergroup_id !== undefined && channel_id !== undefined) {
       const message_thread_id = parseInt(channel_id);
-      const chat_id = parseInt(supergroup_id)
+      const chat_id = parseInt(supergroup_id);
       if (!isNaN(message_thread_id) && !isNaN(chat_id)) {
         await bot.api.sendMessage(chat_id, message, {
           message_thread_id,
@@ -25,3 +28,53 @@ export async function sendMessage(message: string, bot?: Bot) {
     }
   }
 }
+
+export const formatPollCreated = (ballot: Ballot, polls: Poll[]) => {
+  const formatPolls = (polls: Poll[]) => {
+    let pollStr = "";
+    for (const poll of polls) {
+      pollStr += `<i>${poll.body}</i>\n`;
+    }
+    return pollStr;
+  };
+  console.log(`polls`, polls);
+  let ballotPost =
+    ballot.ballotType === BallotType.STRAWPOLL
+      ? "New straw poll posted!"
+      : "New advisory vote posted!";
+  ballotPost =
+    ballotPost +
+    `\n\nTitle: <b>${cleanString(ballot.ballotTitle)}</b>` +
+    `\nDescription: <i>${cleanString(ballot.ballotDescription)}</i>` +
+    `\nQuestions:\n${formatPolls(polls)}` +
+    `\nExpiry: <i>${new Date(ballot.expiry).toLocaleString("en-US", {
+      timeZone: "Asia/Istanbul",
+    })}</i>` +
+    `\n\n<a href="${SITE_URL}/ballot?id=${ballot.ballotURL}">Vote/View</a>`;
+  console.log(`Post`, ballotPost);
+  return ballotPost;
+};
+
+const question = "What's your favorite fruit?";
+const options = ["Apple", "Banana", "Cherry"];
+const votes = [5, 3, 7];
+
+function generatePollHTML(ballot: string, options: string[], votes: number[]) {
+  const totalVotes = votes.reduce((a, b) => a + b, 0);
+
+  let html = `<b>${question}</b> - <i>${totalVotes} votes </i>\n\n`;
+
+  for (let i = 0; i < options.length; i++) {
+    const percentage = (votes[i] / totalVotes) * 100;
+    const rounded = Math.round(percentage / 10);
+    const numWhite = 10 - rounded;
+
+    html += `<i>${options[i]}:</i>\n\n`;
+    html += `${`🟦`.repeat(rounded) + `⬜️`.repeat(numWhite)}`;
+    html += ` (${percentage.toFixed(2)}%)\n\n`;
+  }
+
+  return html;
+}
+
+export const voteStr = generatePollHTML(question, options, votes);
