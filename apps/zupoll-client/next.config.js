c6e8631274ffc0032363264affb76bb1a9f7a4d0
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-30 15:59:22 +0300

    chore: revert to old config

--- apps/zupoll-client/next.config.js
+++ apps/zupoll-client/next.config.js
@@ -1,18 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  webpack(config, { nextRuntime }) {
-    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
-    if (typeof nextRuntime === "undefined") {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        readline: false,
-      };
-    }
-
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: [],
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+    };
     return config;
   },
 };
-
-module.exports = nextConfig;
