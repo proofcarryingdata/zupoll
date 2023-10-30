3ab56974c1594408f88396be61cc381faeab02e0
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-30 12:13:15 +0300

    chore: stash apply

--- apps/zupoll-client/next.config.js
+++ apps/zupoll-client/next.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: [],
-  compiler: {
-    styledComponents: true,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  webpack(config, { nextRuntime }) {
+    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+    if (typeof nextRuntime === "undefined") {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        readline: false,
+      };
+    }
+
     return config;
   },
 };
+
+module.exports = nextConfig;
