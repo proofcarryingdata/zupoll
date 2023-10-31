ac2cc733a91c729a7f1cd2962e717cb471672790
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-31 14:29:45 +0300

    chore: formatting

--- apps/zupoll-server/src/util/bot.ts
+++ apps/zupoll-server/src/util/bot.ts
@@ -55,7 +55,7 @@ export const formatPollCreated = (ballot: Ballot, polls: Poll[]) => {
     ballotPost += `\n\n<a href="${process.env.BOT_ZUPOLL_LINK}?startapp=${ballot.ballotURL}">Vote</a>`;
   }
 
-  ballotPost += `\n\n<a href="${SITE_URL}ballot?id=${ballot.ballotURL}">Browser link</a>`;
+  ballotPost += `\n\n<a href="${SITE_URL}ballot?id=${ballot.ballotURL}">(Vote via browser)</a>`;
 
   console.log(`Post`, ballotPost);
   return ballotPost;
