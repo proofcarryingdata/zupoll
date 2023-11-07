e707781ab7f778d05c33fbdbca8e5ac9fe4778ef
cha0sg0d <42984734+cha0sg0d@users.noreply.github.com>
2023-11-07 05:41:46 -0800

    Fix Prisma (#85)
    * feat: again
    
    * chore: 15 min cron

--- apps/zupoll-server/src/util/prisma.ts
+++ apps/zupoll-server/src/util/prisma.ts
@@ -1,24 +1,7 @@
-/* eslint-disable no-var */
-
 import { PrismaClient } from "@prisma/client";
 
-// This is to prevent prisma clients from accumulating during hot reloading
-// https://github.com/prisma/prisma/issues/1983
-// https://github.com/facebook/jest/issues/11640
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-declare global {
-  var prisma: PrismaClient;
-}
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-function getPrismaClient() {
-  if (process.env.NODE_ENV === "production") {
-    return new PrismaClient();
-  } else if (global.prisma instanceof PrismaClient) {
-    return global.prisma;
-  } else {
-    global.prisma = new PrismaClient();
-    return global.prisma;
-  }
-}
-
-export const prisma = getPrismaClient();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
